0d18349f64f7a289709fc2cdc6e305598c8ea522
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-25 08:34:17 -0300

    feat: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { HomeContainer } from "./styles";
+
+export function Home() {
+  return (
+    <div>
+      <HomeContainer>
+        <h1>Hello Home</h1>
+      </HomeContainer>
+    </div>
+  );
+}
