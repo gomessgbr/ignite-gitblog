d166b18be0de453fe87f745f47c32a24632d72a6
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 14:36:46 -0300

    font awesome lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,35 @@
-import { HomeContainer } from "./styles";
+import { HomeContainer, HomeProfile, PostCards, SearchInput } from "./styles";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export function Home() {
   console.log("Eu to aqui!");
   return (
-    <div>
-      <HomeContainer>
-        <h1 >Hello Home</h1>
-      </HomeContainer>
-    </div>
+    <HomeContainer>
+      <HomeProfile>
+        <img src="" alt="" />
+        <div>
+          <h1>Cameron Williamson</h1>
+          <span>
+            github <a href=""></a>
+          </span>
+        </div>
+        <div>
+          <p>
+            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+            volutpat pulvinar vel mass.
+          </p>
+          <div>
+            <FontAwesomeIcon icon={faGithub} />
+            <a href="">cameronwll</a>
+            <a href="">Rocketseat</a>
+            <a href=""> 32 seguidores</a>
+          </div>
+        </div>
+      </HomeProfile>
+    </HomeContainer>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,8 +2,40 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HomeProfile = styled.div`
+  color: ${({ theme }) => theme.white};
   h1 {
-    font-size: 42;
-    color: white;
+    color: ${({ theme }) => theme["base-title"]};
+    font-weight: 700;
+    line-height: 130%;
+    font-size: 1.5rem;
+  }
+  span {
+    color: ${({ theme }) => theme.blue};
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 0.75rem;
+    line-height: 160%;
+  }
+  p {
+    color: ${({ theme }) => theme["base-text"]};
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+  }
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme["base-subtitle"]};
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
   }
 `;
+
+export const SearchInput = styled.input``;
+
+export const PostCards = styled.div``;
