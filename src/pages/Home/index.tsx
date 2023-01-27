d9e4fd46740ed39077b137f3cc80b1a820522ec5
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 12:32:53 -0300

    feat: make a home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import { HomeContainer } from "./styles";
 
 export function Home() {
+  console.log("Eu to aqui!");
   return (
     <div>
       <HomeContainer>
-        <h1>Hello Home</h1>
+        <h1 >Hello Home</h1>
       </HomeContainer>
     </div>
   );
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  display: flex;
+  h1 {
+    font-size: 42;
+    color: white;
+  }
+`;
