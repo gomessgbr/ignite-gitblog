d13bd43e57fd8584a117264f17b3ca91e3c87b48
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 15:46:35 -0300

    feat: add Profile on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,59 @@
-import { HomeContainer, HomeProfile, PostCards, SearchInput } from "./styles";
+import {
+  HomeContainer,
+  HomeProfile,
+  HomeProfileWrapper,
+  HomeProfileTitle,
+  HomeProfileSubtitle,
+  FooterSubtitleIcons,
+  PostCards,
+  SearchInput,
+} from "./styles";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import {
+  faBuilding,
+  faUserGroup,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
+import ProfileImage from "../../assets/avatar.png";
+
 export function Home() {
-  console.log("Eu to aqui!");
   return (
     <HomeContainer>
       <HomeProfile>
-        <img src="" alt="" />
-        <div>
-          <h1>Cameron Williamson</h1>
-          <span>
-            github <a href=""></a>
-          </span>
-        </div>
-        <div>
-          <p>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
-          <div>
-            <FontAwesomeIcon icon={faGithub} />
-            <a href="">cameronwll</a>
-            <a href="">Rocketseat</a>
-            <a href=""> 32 seguidores</a>
-          </div>
-        </div>
+        <img src={ProfileImage} alt="Imagem do perfil" />
+        <HomeProfileWrapper>
+          <HomeProfileTitle>
+            <h1>Cameron Williamson</h1>
+            <span>
+              github <a href=""></a>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </span>
+          </HomeProfileTitle>
+          <HomeProfileSubtitle>
+            <p>
+              Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+              viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+              volutpat pulvinar vel mass.
+            </p>
+            <FooterSubtitleIcons>
+              <div>
+                <FontAwesomeIcon icon={faGithub} />
+                <a href="">cameronwll</a>
+              </div>
+              <div>
+                <FontAwesomeIcon icon={faBuilding} />
+                <a href="">Rocketseat</a>
+              </div>
+              <div>
+                <FontAwesomeIcon icon={faUserGroup} />
+                <a href=""> 32 seguidores</a>
+              </div>
+            </FooterSubtitleIcons>
+          </HomeProfileSubtitle>
+        </HomeProfileWrapper>
       </HomeProfile>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,12 +7,41 @@ export const HomeContainer = styled.div`
 `;
 
 export const HomeProfile = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 13.25rem;
+  max-width: 54rem;
+
+  align-items: center;
+  justify-content: center;
+  margin-top: -5rem;
+
+  border-radius: 10px;
+  background: ${({ theme }) => theme["base-profile"]};
   color: ${({ theme }) => theme.white};
+
+  img {
+    margin: 2rem 2rem 2rem 2.5rem;
+  }
+`;
+
+export const HomeProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HomeProfileTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 19.32rem;
+
   h1 {
     color: ${({ theme }) => theme["base-title"]};
     font-weight: 700;
     line-height: 130%;
     font-size: 1.5rem;
+    margin-bottom: 0.5rem;
   }
   span {
     color: ${({ theme }) => theme.blue};
@@ -20,19 +49,36 @@ export const HomeProfile = styled.div`
     font-weight: 700;
     font-size: 0.75rem;
     line-height: 160%;
+    margin-bottom: 1.25rem;
   }
+`;
+
+export const HomeProfileSubtitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
   p {
     color: ${({ theme }) => theme["base-text"]};
     font-weight: 400;
     font-size: 1rem;
     line-height: 160%;
   }
+`;
+
+export const FooterSubtitleIcons = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 1.5rem;
+
   a {
     text-decoration: none;
     color: ${({ theme }) => theme["base-subtitle"]};
     font-weight: 400;
     font-size: 1rem;
     line-height: 160%;
+    margin-left: 0.5rem;
   }
 `;
 
