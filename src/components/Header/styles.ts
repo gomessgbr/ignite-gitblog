c3773c8594631634957c0dcd4f55ee29d6527fb2
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 12:54:23 -0300

    feat: center an header image

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div``;
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 18.75rem;
+
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+  }
+`;
