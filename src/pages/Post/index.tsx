387ee34b9efa2689302a94eef8acbd8affcd02f0
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 20:35:29 -0300

    feat: create a postPage

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,102 @@
-import { PostContainer } from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faArrowUpRightFromSquare,
+  faCalendarDay,
+  faComment,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import {
+  PostContainer,
+  PostTitleWrapper,
+  PostContentWrapper,
+  PostTitleHeaderIconsWrapper,
+  PostTitleFooterIconsWrapper,
+  PostTitleContentWrapper,
+  PostContent,
+} from "./styles";
 
 export function Post() {
   return (
     <div>
       <PostContainer>
-        <h1>Hello Post</h1>
+        <PostTitleWrapper>
+          <PostTitleHeaderIconsWrapper>
+            <Link to="/">
+              <FontAwesomeIcon
+                icon={faChevronLeft}
+                style={{ marginRight: "0.5rem" }}
+              />
+              Voltar
+            </Link>
+            <span>
+              <a href="">
+                Ver No github{" "}
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  style={{ marginLeft: "0.505rem" }}
+                />
+              </a>
+            </span>
+          </PostTitleHeaderIconsWrapper>
+          <PostTitleContentWrapper>
+            <h1>JavaScript data types and data structures</h1>
+            <PostTitleFooterIconsWrapper>
+              <div>
+                <span>
+                  <FontAwesomeIcon icon={faGithub} />
+                  cameronwll
+                </span>
+              </div>
+              <div>
+                <span>
+                  <FontAwesomeIcon icon={faCalendarDay} />
+                  Rocketseat
+                </span>
+              </div>
+              <div>
+                <span>
+                  <FontAwesomeIcon icon={faComment} />
+                  32 seguidores
+                </span>
+              </div>
+            </PostTitleFooterIconsWrapper>
+          </PostTitleContentWrapper>
+        </PostTitleWrapper>
+        <PostContentWrapper>
+          <PostContent>
+            <p>
+              <b>
+                Programming languages all have built-in data structures, but
+                these often differ from one language to another.
+              </b>
+              This article attempts to list the built-in data structures
+              available in JavaScript and what properties they have. These can
+              be used to build other data structures. Wherever possible,
+              comparisons with other languages are drawn.
+            </p>
+
+            <a href="" style={{ color: "#3294F8" }}>
+              Dynamic typing
+            </a>
+            <p>
+              JavaScript is a loosely typed and dynamic language. Variables in
+              JavaScript are not directly associated with any particular value
+              type, and any variable can be assigned (and re-assigned) values of
+              all types:
+            </p>
+
+            {/* <p>
+              ```javascript 
+              let foo = 42; // foo is now a number 
+              foo = ‘bar’; //foo is now a string 
+              foo = true; // foo is now a boolean 
+              
+              ```
+            </p> */}
+          </PostContent>
+        </PostContentWrapper>
       </PostContainer>
     </div>
   );
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,3 +1,89 @@
 import styled from "styled-components";
 
-export const PostContainer = styled.div``;
+export const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PostTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 10.5rem;
+  width: 54rem;
+  background: ${({ theme }) => theme["base-profile"]};
+  margin-top: -5rem;
+
+  border-radius: 10px;
+  gap: 1.25rem;
+  padding: 2rem;
+`;
+
+export const PostTitleHeaderIconsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 38.625rem;
+  a {
+    color: ${({ theme }) => theme.blue};
+    text-decoration: none;
+    text-transform: uppercase;
+
+    font-weight: 700;
+    font-size: 0.75rem;
+    line-height: 160%;
+  }
+`;
+
+export const PostTitleContentWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+
+  gap: 0.5rem;
+
+  h1 {
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 130%;
+    color: ${({ theme }) => theme["base-title"]};
+  }
+`;
+
+export const PostTitleFooterIconsWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+
+  span {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 160%;
+
+    color: ${({ theme }) => theme["base-span"]};
+  }
+`;
+
+export const PostContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 54rem;
+  min-height: 27rem;
+`;
+
+export const PostContent = styled.div`
+  padding: 2.5rem 2rem;
+  p {
+    color: ${({ theme }) => theme["base-text"]};
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 160%;
+  }
+`;
