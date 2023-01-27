c89d9a685b85b9ca334c30ab889ee0e0f7bf3209
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 12:33:09 -0300

    feat: make a post page

---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,11 @@
+import { PostContainer } from "./styles";
+
+export function Post() {
+  return (
+    <div>
+      <PostContainer>
+        <h1>Hello Post</h1>
+      </PostContainer>
+    </div>
+  );
+}
