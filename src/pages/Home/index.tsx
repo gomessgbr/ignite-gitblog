a0d0f073932817dfb601342c82d1133467accd8d
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-27 18:33:08 -0300

    feat: create postcard and search input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,8 +5,13 @@ import {
   HomeProfileTitle,
   HomeProfileSubtitle,
   FooterSubtitleIcons,
-  PostCards,
+  SearchInputContainer,
+  SearchInputTitleWrapper,
   SearchInput,
+  PostCardsContainer,
+  PostCardContent,
+  PostCardContentTitle,
+  PostCardContentSubTitle,
 } from "./styles";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -55,6 +60,145 @@ export function Home() {
           </HomeProfileSubtitle>
         </HomeProfileWrapper>
       </HomeProfile>
+
+      <SearchInputContainer>
+        <SearchInputTitleWrapper>
+          <span className="publics">Publicações</span>
+          <span className="amountPublics">6 publicações</span>
+        </SearchInputTitleWrapper>
+        <SearchInput placeholder="Buscar conteúdo"></SearchInput>
+      </SearchInputContainer>
+
+      <PostCardsContainer>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+        <PostCardContent>
+          <PostCardContentTitle>
+            <div style={{ maxWidth: "17.69rem" }}>
+              <h1>JavaScript data types and data structures</h1>
+            </div>
+
+            <span>Há 1 dia</span>
+          </PostCardContentTitle>
+          <PostCardContentSubTitle>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in...{" "}
+            </p>
+          </PostCardContentSubTitle>
+        </PostCardContent>
+      </PostCardsContainer>
     </HomeContainer>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
@@ -82,6 +83,109 @@ export const FooterSubtitleIcons = styled.div`
   }
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 4.5rem;
+`;
 
-export const PostCards = styled.div``;
+export const SearchInputTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 42.44rem;
+
+  .publics {
+    font-weight: 700;
+    font-size: 1.125rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme["base-subtitle"]};
+    margin-bottom: 0.75rem;
+  }
+  .amountPublics {
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme["base-span"]};
+    text-align: right;
+    margin-top: 0.25rem;
+    margin-bottom: 0.94rem;
+  }
+`;
+
+export const SearchInput = styled.input`
+  width: 54rem;
+  height: 3.125rem;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.white};
+
+  background: ${({ theme }) => theme["base-input"]};
+
+  border: 1px solid ${({ theme }) => theme["base-border"]};
+  border-radius: 6px;
+
+  padding: 0.75rem 1rem;
+
+  ::placeholder {
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+
+    color: ${({ theme }) => theme["base-label"]};
+  }
+`;
+
+export const PostCardsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 3rem;
+  margin-bottom: 14.625rem;
+  gap: 2rem;
+  max-height: 16.25rem;
+`;
+
+export const PostCardContent = styled.div`
+  width: 26rem;
+  height: 16.25rem;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+
+  background: ${({ theme }) => theme["base-post"]};
+
+  border-radius: 10px;
+`;
+
+export const PostCardContentTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1.25rem;
+  gap: 1rem;
+
+  h1 {
+    font-weight: 700;
+    font-size: 1.25rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme["base-title"]};
+  }
+
+  span {
+    font-weight: 400;
+    font-size: 0.875rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme["base-span"]};
+  }
+`;
+
+export const PostCardContentSubTitle = styled.div`
+  max-height: 7rem;
+  overflow-y: hidden;
+  p {
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    color: ${({ theme }) => theme["base-text"]};
+  }
+`;
