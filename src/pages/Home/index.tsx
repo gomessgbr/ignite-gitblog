3f3aed0e97dbc47d592b001d597924685ac53cd9
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-30 21:26:28 -0300

    refactor: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,5 @@
 import {
   HomeContainer,
-  HomeProfile,
-  HomeProfileWrapper,
-  HomeProfileTitle,
-  HomeProfileSubtitle,
-  FooterSubtitleIcons,
   SearchInputContainer,
   SearchInputTitleWrapper,
   SearchInput,
@@ -14,54 +9,13 @@ import {
   PostCardContentSubTitle,
 } from "./styles";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBuilding,
-  faUserGroup,
-  faArrowUpRightFromSquare,
-} from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
-
-import ProfileImage from "../../assets/avatar.png";
+import { HomeProfile } from "./components/HomeProfile";
 
 export function Home() {
   return (
     <HomeContainer>
-      <HomeProfile>
-        <img src={ProfileImage} alt="Imagem do perfil" />
-        <HomeProfileWrapper>
-          <HomeProfileTitle>
-            <h1>Cameron Williamson</h1>
-            <span>
-              github{"    "}
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-            </span>
-          </HomeProfileTitle>
-          <HomeProfileSubtitle>
-            <p>
-              Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-              viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-              volutpat pulvinar vel mass.
-            </p>
-            <FooterSubtitleIcons>
-              <div>
-                <FontAwesomeIcon icon={faGithub} />
-                <a href="">cameronwll</a>
-              </div>
-              <div>
-                <FontAwesomeIcon icon={faBuilding} />
-                <a href="">Rocketseat</a>
-              </div>
-              <div>
-                <FontAwesomeIcon icon={faUserGroup} />
-                <a href=""> 32 seguidores</a>
-              </div>
-            </FooterSubtitleIcons>
-          </HomeProfileSubtitle>
-        </HomeProfileWrapper>
-      </HomeProfile>
-
+      <HomeProfile />
       <SearchInputContainer>
         <SearchInputTitleWrapper>
           <span className="publics">Publicações</span>
