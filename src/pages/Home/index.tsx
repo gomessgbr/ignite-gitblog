cad72dbc0c961766635e0fc96bcdab8158cb0af6
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-01-31 17:08:11 -0300

    refactor: change post for component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,161 +1,85 @@
-import {
-  HomeContainer,
-  SearchInputContainer,
-  SearchInputTitleWrapper,
-  SearchInput,
-  PostCardsContainer,
-  PostCardContent,
-  PostCardContentTitle,
-  PostCardContentSubTitle,
-} from "./styles";
+import { HomeContainer, PostCardsContainer } from "./styles";
 
-import { Link } from "react-router-dom";
 import { HomeProfile } from "./components/HomeProfile";
+import { Search } from "./components/Search";
+import { PostCards } from "./components/PostCards";
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface IPost {
+  title: string;
+  number: number;
+  body: string;
+  updated_at: string;
+}
+[];
 
 export function Home() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  async function fetchPosts() {
+    const response = await api.get("/search/issues", {
+      params: {
+        q: "repo:gomessgbr/ignite-gitblog",
+      },
+    });
+
+    const responseDatas = response.data.items;
+
+    const postDatas: IPost[] = [];
+
+    responseDatas.map(
+      (post: {
+        title: string;
+        number: number;
+        body: string;
+        updated_at: string;
+      }) => {
+        const dateToDate = new Date(post.updated_at);
+        const convertDate = formatDistanceToNow(dateToDate, {
+          locale: ptBR,
+          addSuffix: true,
+        });
+
+        const fetchDatas = {
+          title: post.title,
+          number: post.number,
+          body: post.body,
+          updated_at: convertDate,
+        };
+
+        return postDatas.push({ ...fetchDatas });
+      }
+    );
+
+    setPosts(postDatas);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  let countPosts = posts.length;
+
   return (
     <HomeContainer>
       <HomeProfile />
-      <SearchInputContainer>
-        <SearchInputTitleWrapper>
-          <span className="publics">Publicações</span>
-          <span className="amountPublics">6 publicações</span>
-        </SearchInputTitleWrapper>
-        <SearchInput placeholder="Buscar conteúdo"></SearchInput>
-      </SearchInputContainer>
-
+      <Search amountOfPosts={countPosts} />
       <PostCardsContainer>
-        <PostCardContent>
-          <Link to="Post" title="Post">
-            <PostCardContentTitle>
-              <div style={{ maxWidth: "17.69rem" }}>
-                <h1>JavaScript data types and data structures</h1>
-              </div>
-
-              <span>Há 1 dia</span>
-            </PostCardContentTitle>
-            <PostCardContentSubTitle>
-              <p>
-                Programming languages all have built-in data structures, but
-                these often differ from one language to another. This article
-                attempts to list the built-in data structures available in...{" "}
-              </p>
-            </PostCardContentSubTitle>
-          </Link>
-        </PostCardContent>
-
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
-        <PostCardContent>
-          <PostCardContentTitle>
-            <div style={{ maxWidth: "17.69rem" }}>
-              <h1>JavaScript data types and data structures</h1>
-            </div>
-
-            <span>Há 1 dia</span>
-          </PostCardContentTitle>
-          <PostCardContentSubTitle>
-            <p>
-              Programming languages all have built-in data structures, but these
-              often differ from one language to another. This article attempts
-              to list the built-in data structures available in...{" "}
-            </p>
-          </PostCardContentSubTitle>
-        </PostCardContent>
+        {posts.map((post) => {
+          return (
+            <PostCards
+              key={`${post.title}+${post.number}`}
+              titlePost={post.title}
+              bodyPost={post.body}
+              numberPost={post.number}
+              updated_at={post.updated_at}
+            />
+          );
+        })}
       </PostCardsContainer>
     </HomeContainer>
   );
