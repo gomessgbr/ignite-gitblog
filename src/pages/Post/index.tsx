0e3fa6914f020e63405fbafb9f4ece5dedcf4a8e
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-02-01 14:19:23 -0300

    feat: mapping post content get github api

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {
@@ -6,7 +8,7 @@ import {
   faComment,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
 import {
   PostContainer,
   PostTitleWrapper,
@@ -17,78 +19,99 @@ import {
   PostContent,
 } from "./styles";
 
+interface IPostContent {
+  postLink: string;
+  title: string;
+  content: string;
+
+  comments: string;
+  tagUser: string;
+}
+
 export function Post() {
+  const [postContent, setPostContent] = useState<IPostContent[]>([]);
+  let { number } = useParams();
+  const { state } = useLocation();
+  async function sendtoPostPage() {
+    const response = await api.get(
+      `/repos/gomessgbr/ignite-gitblog/issues/${number}`
+    );
+
+    const postFetched = [
+      {
+        postLink: response?.data?.html_url,
+        title: response?.data?.title,
+        content: response?.data?.body,
+
+        comments: response?.data?.comments,
+        tagUser: response?.data?.user.login,
+      },
+    ];
+
+    setPostContent(postFetched);
+  }
+
+  useEffect(() => {
+    sendtoPostPage();
+  }, []);
+
   return (
     <div>
-      <PostContainer>
-        <PostTitleWrapper>
-          <PostTitleHeaderIconsWrapper>
-            <Link to="/">
-              <FontAwesomeIcon
-                icon={faChevronLeft}
-                style={{ marginRight: "0.5rem" }}
-              />
-              Voltar
-            </Link>
-            <span>
-              <a href="">
-                Ver No github{" "}
-                <FontAwesomeIcon
-                  icon={faArrowUpRightFromSquare}
-                  style={{ marginLeft: "0.505rem" }}
-                />
-              </a>
-            </span>
-          </PostTitleHeaderIconsWrapper>
-          <PostTitleContentWrapper>
-            <h1>JavaScript data types and data structures</h1>
-            <PostTitleFooterIconsWrapper>
-              <div>
-                <span>
-                  <FontAwesomeIcon icon={faGithub} />
-                  cameronwll
-                </span>
-              </div>
-              <div>
-                <span>
-                  <FontAwesomeIcon icon={faCalendarDay} />
-                  Rocketseat
-                </span>
-              </div>
-              <div>
-                <span>
-                  <FontAwesomeIcon icon={faComment} />
-                  32 seguidores
-                </span>
-              </div>
-            </PostTitleFooterIconsWrapper>
-          </PostTitleContentWrapper>
-        </PostTitleWrapper>
-        <PostContentWrapper>
-          <PostContent>
-            <p>
-              <b>
-                Programming languages all have built-in data structures, but
-                these often differ from one language to another.
-              </b>
-              This article attempts to list the built-in data structures
-              available in JavaScript and what properties they have. These can
-              be used to build other data structures. Wherever possible,
-              comparisons with other languages are drawn.
-            </p>
-
-            <a href="" style={{ color: "#3294F8" }}>
-              Dynamic typing
-            </a>
-            <p>
-              JavaScript is a loosely typed and dynamic language. Variables in
-              JavaScript are not directly associated with any particular value
-              type, and any variable can be assigned (and re-assigned) values of
-              all types:
-            </p>
-          </PostContent>
-        </PostContentWrapper>
-      </PostContainer>
+      {postContent &&
+        postContent.map((content) => {
+          return (
+            <PostContainer key={content.postLink}>
+              <PostTitleWrapper>
+                <PostTitleHeaderIconsWrapper>
+                  <Link to="/">
+                    <FontAwesomeIcon
+                      icon={faChevronLeft}
+                      style={{ marginRight: "0.5rem" }}
+                    />
+                    Voltar
+                  </Link>
+                  <span>
+                    <a href={content.postLink}>
+                      Ver No github{" "}
+                      <FontAwesomeIcon
+                        icon={faArrowUpRightFromSquare}
+                        style={{ marginLeft: "0.505rem" }}
+                      />
+                    </a>
+                  </span>
+                </PostTitleHeaderIconsWrapper>
+                <PostTitleContentWrapper>
+                  <h1>{content.title}</h1>
+                  <PostTitleFooterIconsWrapper>
+                    <div>
+                      <span>
+                        <FontAwesomeIcon icon={faGithub} />
+                        {content.tagUser}
+                      </span>
+                    </div>
+                    <div>
+                      <span>
+                        <FontAwesomeIcon icon={faCalendarDay} />
+                        {state.updated_at}
+                      </span>
+                    </div>
+                    <div>
+                      <span>
+                        <FontAwesomeIcon icon={faComment} />
+                        {content.comments} comentários
+                      </span>
+                    </div>
+                  </PostTitleFooterIconsWrapper>
+                </PostTitleContentWrapper>
+              </PostTitleWrapper>
+              <PostContentWrapper>
+                <PostContent>
+                  <p>{content.content}</p>
+                </PostContent>
+              </PostContentWrapper>
+            </PostContainer>
+          );
+        })}
     </div>
   );
 }
